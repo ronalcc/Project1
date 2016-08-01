58fc099ba37df803b26d22a7dace2acd849006ff
chenchen <ronalcc@126.com>
2016-08-01 13:46:53 +0800

    Initial commit

---
+++ app/HomePage.js
@@ -0,0 +1,5 @@
+/**
+ * Created by lenovo on 2016/8/1.
+ */
+'use strict';
+
---
+++ index.android.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import  Header from './app/Header'
+
+AppRegistry.registerComponent('Project1', () => Header);
